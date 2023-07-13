1331b04a458c554dacd04dd360d873ec5770d8bc
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-14 08:45:15 +1000

    Fix padding of buttons on mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,8 +8,8 @@
                     <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">The reverse job board for developers in Australia</h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600">Find your next developer opportunity in Australia. Create a profile and let companies come to you.</p>
                     <div class="mt-5 flex items-center justify-center gap-x-6">
-                        <a href="#" class="bg-gray-900 text-white rounded-md px-4 py-3 text-sm font-medium">Get started</a>
-                        <a href="{{ route('developers') }}" class="text-gray-900 border border-gray-900 rounded-md px-4 py-3 text-sm font-medium">Developers</a>
+                        <a href="#" class="bg-gray-900 text-white rounded-md px-3 py-2 sm:px-4 sm:py-3 text-sm font-medium">Get started</a>
+                        <a href="{{ route('developers') }}" class="text-gray-900 border border-gray-900 rounded-md px-3 py-2 sm:px-4 sm:py-3 text-sm font-medium">Developers</a>
                     </div>
                 </div>
             </div>
