5c11db6ec99cb56217dfb4124037fb173f53b66e
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-16 09:51:50 +1000

    Update navbar logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -15,9 +15,9 @@
             </div>
             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <a href="{{ route('home') }}" class="flex flex-shrink-0 items-center">
-                    <img class="h-22 w-88 w-auto" src="{{ url('img/ozdevs-logo-two.png') }}" alt="OzDevs">
+                    <img style="width: 88px; height: 22px;" src="{{ url('img/ozdevs-logo-two.png') }}" alt="OzDevs">
                 </a>
-                <div class="hidden sm:ml-6 sm:block">
+                <div class="hidden sm:ml-3 sm:block">
                     <div class="flex space-x-4">
                         <a href="{{ route('developers') }}" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Developers</a>
                         <a href="" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Pricing</a>
