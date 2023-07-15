2c026d720e753d757f2f5117f365e1e38e218f82
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-15 14:29:44 +1000

    Setup flowbite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
 
         <title>OzDevs</title>
 
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css','resources/js/app.js'])
+
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
