8f6f243ae719c32f13bef2356d52f00b249b2e4b
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-16 11:11:42 +1000

    Add styles to filters

--- app/Http/Controllers/DevelopersController.php
+++ app/Http/Controllers/DevelopersController.php
@@ -7,12 +7,28 @@ use Illuminate\Http\Request;
 
 class DevelopersController extends Controller
 {
-    public function index() {
+    public function index(Request $request)
+    {
+        $query = DeveloperProfile::query();
 
-        $developers = DeveloperProfile::paginate(15);
+        // Apply filters if provided
+        if ($request->filled('city')) {
+            $query->where('city', $request->input('city'));
+        }
+
+        if ($request->filled('experience_level')) {
+            $query->where('role_level', $request->input('experience_level'));
+        }
+
+        // Retrieve filtered developers with pagination
+        $developers = $query->paginate(15);
+
+        // Append the query parameters to the pagination links
+        $developers->appends($request->query());
 
         return view("developers", [
             'developers' => $developers,
         ]);
     }
+
 }
--- resources/views/developers.blade.php
+++ resources/views/developers.blade.php
@@ -4,10 +4,60 @@
     <div class="container mx-auto mt-10">
         <h2 class="text-xl lg:text-2xl mb-5">Find developers in Australia</h2>
 
-        <div class="flex flex-wrap gap-y-6">
-            @foreach ($developers as $developer)
-                <x-developer-card :developer="$developer"></x-developer-card>
-            @endforeach
+        <p class="text-gray-500 mb-4">Showing {{ $developers->firstItem() }} to {{ $developers->lastItem() }} of {{ $developers->total() }} developers</p>
+
+
+        <div class="flex flex-col lg:flex-row">
+            <!-- Filters column -->
+            <div class="w-full lg:w-1/5 mr-4">
+                <form action="{{ route('developers') }}" method="GET">
+
+                    <!-- Search bar -->
+                    <div class="mb-4">
+                        <label for="search" class="block text-sm font-medium text-gray-700">Search</label>
+                        <input type="text" id="search" class="mt-1 px-3 py-2 border border-gray-300 rounded-md w-full" name="search" value="{{ request('search') }}">
+                    </div>
+
+                    <!-- Filter by city -->
+                    <div class="mb-4">
+                        <label for="city" class="block text-sm font-medium text-gray-700">City</label>
+                        <select id="city" class="mt-1 px-3 py-2 border border-gray-300 rounded-md w-full" name="city">
+                            <option value="">All Cities</option>
+                            <option value="sydney" {{ request('city') === 'sydney' ? 'selected' : '' }}>Sydney</option>
+                            <option value="melbourne" {{ request('city') === 'melbourne' ? 'selected' : '' }}>Melbourne</option>
+                            <option value="brisbane" {{ request('city') === 'brisbane' ? 'selected' : '' }}>Brisbane</option>
+                            <!-- Add more city options here -->
+                        </select>
+                    </div>
+
+                    <!-- Filter by experience level -->
+                    <div class="mb-4">
+                        <label for="experience" class="block text-sm font-medium text-gray-700">Experience Level</label>
+                        <select id="experience" class="mt-1 px-3 py-2 border border-gray-300 rounded-md w-full" name="experience_level">
+                            <option value="">All Experience Levels</option>
+                            <option value="junior" {{ request('experience_level') === 'junior' ? 'selected' : '' }}>Junior</option>
+                            <option value="intermediate" {{ request('experience_level') === 'intermediate' ? 'selected' : '' }}>Intermediate</option>
+                            <option value="senior" {{ request('experience_level') === 'senior' ? 'selected' : '' }}>Senior</option>
+                            <!-- Add more experience level options here -->
+                        </select>
+                    </div>
+
+                    <!-- Add more filters here -->
+
+                    <!-- Apply and Clear buttons -->
+                    <div class="space-x-0 lg:space-x-2 mb-10">
+                        <button type="submit" class="w-full lg:w-auto text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">Apply</button>
+                        <a href="{{ route('developers') }}" class="w-full lg:w-auto py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Clear</a>
+                    </div>
+                </form>
+            </div>
+
+            <!-- Developers cards column -->
+            <div class="flex flex-wrap w-full lg:w-4/5 gap-y-6" style="height: fit-content;">
+                @foreach ($developers as $developer)
+                    <x-developer-card :developer="$developer"></x-developer-card>
+                @endforeach
+            </div>
         </div>
 
         <div class="my-5">
