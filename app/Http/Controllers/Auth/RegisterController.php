ab46bc2ef851cdcc48fe392098c22cbdd10c7b09
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-19 19:32:54 +1000

    Log user in after registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
@@ -26,6 +27,9 @@ class RegisterController extends Controller
             'email' => $validatedData['email'],
             'password' => Hash::make($validatedData['password']),
         ]);
+
+        // Log in the user
+        Auth::login($user);
 
         // Redirect the user after successful registration
         return redirect()->route('home')->with('success', 'Registration successful!');
