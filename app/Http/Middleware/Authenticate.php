73f89269b9f60d2cf8b75c52c42572ed8428e1a0
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-23 11:37:23 +1000

    Add flash message auth middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +13,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
+        if (!$request->expectsJson()) {
+            // Add flash message for non-JSON requests
+            Session::flash('warning', 'You need to be logged in to access this page.');
+        }
+
         return $request->expectsJson() ? null : route('login');
     }
 }
