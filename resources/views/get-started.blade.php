eb35a54b1d0e1d27e546f5881c6e7dfa5bbd713a
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-23 13:39:06 +1000

    Styling for get started page

--- resources/views/get-started.blade.php
+++ resources/views/get-started.blade.php
@@ -1,87 +1,80 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="flex flex-col items-center justify-center">
+    <div class="container mx-auto px-4">
         <h1 class="text-4xl font-bold mb-8">Get Started</h1>
-        <div class="flex flex-col md:flex-row gap-4">
-            <div class="w-full md:w-1/2">
-                <div class="p-6 relative flex flex-col h-full overflow-hidden border border-gray-300 rounded-lg justify bg-white">
-                    <div>
-                        <h3 class="text-3xl md:text-3xl pb-3 text-gray-800">Developer</h3>
-                        <ul class="md:text-lg pb-6 pl-1 text-gray-600">
-                            <li class="pb-1">
-                                <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                    </svg>
-                                </span>
-                                <b>Free</b> dev profile.
-                            </li>
-                            <li class="pb-1">
-                                <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                    </svg>
-                                </span>
-                                <b>Free</b> job & project leads (<b>no fees</b>)
-                            </li>
-                            <li class="pb-1">
-                                <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                    </svg>
-                                </span>
-                                Reply to companies.
-                            </li>
-                        </ul>
-                        <a href="{{ route('newDeveloperForm') }}" class="w-full lg:w-auto text-white bg-customBlue hover:bg-customDarkBlue focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2.5 mb-2">
-                            Create Developer Profile
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1 inline-block" viewBox="0 0 24 24">
-                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div class="bg-white rounded-lg shadow-md p-8">
+                <h3 class="text-3xl font-bold text-gray-800 mb-4">Developer</h3>
+                <ul class="text-lg text-gray-600 mb-6">
+                    <li class="mb-3 flex items-center">
+                        <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                <path d="M20 6L9 17l-5-5"></path>
                             </svg>
-                        </a>
-                    </div>
-                </div>
+                        </span>
+                        <b class="mr-1">Free</b> dev profile.
+                    </li>
+                    <li class="mb-3 flex items-center">
+                         <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                <path d="M20 6L9 17l-5-5"></path>
+                            </svg>
+                        </span>
+                        <b class="mr-1">Free</b> job & project leads (<b>no fees</b>).
+                    </li>
+                    <li class="flex items-center">
+                         <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                <path d="M20 6L9 17l-5-5"></path>
+                            </svg>
+                        </span>
+                        Reply to companies.
+                    </li>
+                </ul>
+                <a href="{{ route('newDeveloperForm') }}" class="block text-white bg-customBlue hover:bg-customDarkBlue focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center" style="width: fit-content;">
+                    Create Developer Profile
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1 inline-block" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </a>
             </div>
-            <div class="w-full md:w-1/2 mt-4 md:mt-0">
-                <div class="p-6 relative flex flex-col h-full overflow-hidden border border-gray-300 rounded-lg justify bg-white">
-                    <div>
-                        <h3 class="text-3xl md:text-3xl pb-3 text-gray-800">Company</h3>
-                        <ul class="md:text-lg pb-6 pl-1 text-gray-600">
-                            <li class="pb-1">
-                                <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                    </svg>
-                                </span>
-                                <b>Free</b> company profile.
-                            </li>
-                            <li class="pb-1">
-                                <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                    </svg>
-                                </span>
-                                Access to a pool of talented developers.
-                            </li>
-                            <li class="pb-1">
-                                <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                    </svg>
-                                </span>
-                                Post job & project opportunities.
-                            </li>
-                        </ul>
-                        <a href="" class="w-full lg:w-auto text-white bg-customBlue hover:bg-customDarkBlue focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2.5 mb-2">
-                            Create Company Profile
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1 inline-block" viewBox="0 0 24 24">
-                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+            <div class="bg-white rounded-lg shadow-md p-8">
+                <h3 class="text-3xl font-bold text-gray-800 mb-4">Company</h3>
+                <ul class="text-lg text-gray-600 mb-6">
+                    <li class="mb-3 flex items-center">
+                        <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                <path d="M20 6L9 17l-5-5"></path>
                             </svg>
-                        </a>
-                    </div>
-                </div>
+                        </span>
+                        <b class="mr-1">Free</b> company profile.
+                    </li>
+                    <li class="mb-3 flex items-center">
+                         <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                <path d="M20 6L9 17l-5-5"></path>
+                            </svg>
+                        </span>
+                        Access to a pool of talented developers.
+                    </li>
+                    <li class="flex items-center">
+                         <span class="inline-flex items-center justify-center flex-shrink-0 w-4 h-4 mr-1 text-white bg-green-500 rounded-full">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" class="w-3 h-3" viewBox="0 0 24 24">
+                                <path d="M20 6L9 17l-5-5"></path>
+                            </svg>
+                        </span>
+                        Post job & project opportunities.
+                    </li>
+                </ul>
+                <a href="" class="block text-white bg-customBlue hover:bg-customDarkBlue focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center" style="width: fit-content;">
+                    Create Company Profile
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1 inline-block" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </a>
             </div>
         </div>
     </div>
 @endsection
+
