f38b08e996d027e3ea7817483c30a6763cfeab3e
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-23 18:05:39 +1000

    Update home page svg

--- resources/views/developer-profile.blade.php
+++ resources/views/developer-profile.blade.php
@@ -1,5 +1,36 @@
 @extends("layouts.app")
 
 @section("content")
-    <h1>Developer Profile</h1>
+    <div class="max-w-2xl mx-auto">
+        <!-- Display Developer Profile Information -->
+        <div class="bg-white p-6 rounded-lg shadow-md">
+            <div class="mb-4">
+                <h1 class="text-3xl font-bold mb-8 text-center">{{ $developerProfile->name }}</h1>
+                <p class="text-gray-800 mb-4">City: {{ $developerProfile->city }}</p>
+                <p class="text-gray-800 mb-4">State: {{ $developerProfile->state }}</p>
+                <p class="text-gray-800 mb-4">Country: {{ $developerProfile->country }}</p>
+                <p class="text-gray-800 mb-4">Bio: {{ $developerProfile->bio }}</p>
+                <p class="text-gray-800 mb-4">Search Status: {{ $developerProfile->search_status }}</p>
+                <p class="text-gray-800 mb-4">Role Level: {{ $developerProfile->role_level }}</p>
+                <p class="text-gray-800 mb-4">Employment Type:</p>
+                <ul class="list-disc pl-6 mb-4">
+                    @if($developerProfile->part_time)
+                        <li>Part Time</li>
+                    @endif
+                    @if($developerProfile->full_time)
+                        <li>Full Time</li>
+                    @endif
+                    @if($developerProfile->contract)
+                        <li>Contract</li>
+                    @endif
+                </ul>
+                <p class="text-gray-800 mb-4">Website: {{ $developerProfile->website }}</p>
+                <p class="text-gray-800 mb-4">GitHub: {{ $developerProfile->github }}</p>
+                <p class="text-gray-800 mb-4">Stack Overflow: {{ $developerProfile->stack_overflow }}</p>
+                <p class="text-gray-800 mb-4">LinkedIn: {{ $developerProfile->linkedin }}</p>
+                <p class="text-gray-800 mb-4">Twitter: {{ $developerProfile->twitter }}</p>
+                <p class="text-gray-800 mb-4">Email Notifications: {{ $developerProfile->email_notifications ? 'Yes' : 'No' }}</p>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
     <section class="dark:bg-gray-900">
-        <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Reverse Job Board for Developers in Australia</h1>
                 <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Find your next developer opportunity in Australia. Create a profile and let companies come to you.</p>
@@ -12,7 +12,7 @@
                 </a>
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex justify-end">
-                <img src="{{ url('img/people.svg') }}" class="h-[350px]">
+                <img src="{{ url('img/people-two.svg') }}" class="h-[400px]">
             </div>
         </div>
     </section>
