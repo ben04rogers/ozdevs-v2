412ba4589a6ae60cc39be85c052d377f9afb234a
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-07-23 11:31:25 +1000

    Get started page is publicy accessible

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,7 @@ Route::get('/', [HomeController::class, "index"])->name("home");
 Route::get('/developers', [DevelopersController::class, "index"])->name("developers");
 Route::get('/pricing', [PricingController::class, "index"])->name("pricing");
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/get-started', [GetStartedController::class, "index"])->name("getStarted");
-});
+Route::get('/get-started', [GetStartedController::class, "index"])->name("getStarted");
 
 Route::get('/login', [LoginController::class, "index"])->name("login");
 Route::post('/login', [LoginController::class, "store"])->middleware('web');
@@ -35,5 +33,8 @@ Route::post('/logout', [LoginController::class, "logout"])->name("logout");
 Route::get('/register', [RegisterController::class, "index"])->name("register");
 Route::post('/register', [RegisterController::class, "store"]);
 
-Route::get('/new-developer', [DeveloperProfilesController::class, "index"])->name("newDeveloperForm");
-Route::post('/new-developer', [DeveloperProfilesController::class, "store"])->name("newDeveloper");
+Route::middleware(['auth'])->group(function () {
+    Route::get('/new-developer', [DeveloperProfilesController::class, "index"])->name("newDeveloperForm");
+    Route::post('/new-developer', [DeveloperProfilesController::class, "store"])->name("newDeveloper");
+});
+
