c03a1afa673b783ac8696a7fca2040d5819ff9fe
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-08-03 08:10:24 +1000

    Remove branding of text editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
                     { value: 'First.Name', title: 'First Name' },
                     { value: 'Email', title: 'Email' },
                 ],
+                branding: false,
                 ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
             });
         </script>
