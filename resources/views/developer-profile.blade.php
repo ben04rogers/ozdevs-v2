bb33fe0e73c2fdaeccf0bec002e2da43c1568a96
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-08-10 20:14:39 +1000

    Fix error

--- resources/views/developer-profile.blade.php
+++ resources/views/developer-profile.blade.php
@@ -26,7 +26,7 @@
                 <h1 class="text-2xl md:text-3xl font-bold text-left mb-2">{{ $developerProfile->hero }}</h1>
 
                 <div class="mb-4">
-                    @if (auth()->user()->id === $developerProfile->user_id || auth()->user()?->companyProfile?->paid_subscription)
+                    @if (auth()->check() && $developerProfile && (auth()->user()->id === $developerProfile->user_id || (auth()->user()->companyProfile && auth()->user()->companyProfile->paid_subscription)))
                         <p class="text-xl">{{ $developerProfile->user->name  }}</p>
                     @else
                         <span class="inline-block border-dashed border-2 border-gray-400 bg-gray-200 rounded-lg px-3 py-1.5">
@@ -80,7 +80,7 @@
 
                 <h3 class="text-lg mb-1">Social Links</h3>
 
-                @if (auth()->user()->id === $developerProfile->user_id || auth()->user()?->companyProfile?->paid_subscription)
+                @if (auth()->check() && ($developerProfile->user_id === auth()->user()->id || (auth()->user()->companyProfile && auth()->user()->companyProfile->paid_subscription)))
                     <div class="grid gap-4">
                         <div>
                             @if($developerProfile->website)
