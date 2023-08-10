31beda085616e847baff4c65d1cf699d4d5c2e39
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-08-10 22:19:07 +1000

    Styles for developer profile page

--- resources/views/developer-profile.blade.php
+++ resources/views/developer-profile.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="max-w-2xl mx-auto mb-8">
+    <div class="max-w-5xl mx-auto mb-10">
         <div class="bg-white p-6 rounded-lg shadow-md relative">
             <div class="absolute right-5 top-5">
                 <!-- Display Edit Button for the authenticated user's own profile -->
@@ -9,106 +9,125 @@
                     @if(auth()->user()->id === $developerProfile->user_id)
                         <a href="{{ url('/developer-profiles/' . auth()->user()->developerProfile->id . '/edit') }}" class="inline-block px-4 py-2 bg-customBlue hover:bg-customDarkBlue text-white text-sm rounded-lg">Edit</a>
                     @endif
-
-                        @if(auth()->user()?->companyProfile?->paid_subscription && auth()->user()->id !== $developerProfile->user_id)
-                            <a href="{{ url('/messages/' . $developerProfile->user_id) }}" class="inline-block px-4 py-2 bg-customBlue hover:bg-customDarkBlue text-white text-sm rounded-lg"><i class="fas fa-comment-dots mr-1"></i>Message</a>
-                        @endif
                 @endauth
-
-                @guest
-                    <a href="{{ url('/messages/' . $developerProfile->user_id) }}" class="inline-block px-4 py-2 bg-customBlue hover:bg-customDarkBlue text-white text-sm rounded-lg">Subscribe to Hire</a>
-                @endguest
             </div>
 
-            <div>
+            <div class="peer-focus:scale-75">
                 @if ($developerProfile->image)
                     <img class="rounded w-24 h-24 md:w-32 md:h-32 border mb-5" src="{{$developerProfile->image}}" alt="Extra large avatar">
                 @else
                     <img class="rounded w-24 h-24 md:w-32 md:h-32 border mb-3" id="profileImage" src="{{ asset('img/profile-placeholder.png') }}" alt="Extra large avatar">
                 @endif
+            </div>
 
-                <h1 class="text-2xl md:text-3xl font-bold text-left mb-2">{{ $developerProfile->hero }}</h1>
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-x-4">
+                <div class="col-span- md:col-span-2">
+                    <h1 class="text-2xl md:text-3xl font-bold text-left mb-2">{{ $developerProfile->hero }}</h1>
 
-                <div class="mb-4">
-                    @if (auth()->check() && $developerProfile && (auth()->user()->id === $developerProfile->user_id || (auth()->user()->companyProfile && auth()->user()->companyProfile->paid_subscription)))
-                        <p class="text-xl">{{ $developerProfile->user->name  }}</p>
+                    <div class="mb-4">
+                        @if (auth()->check() && $developerProfile && (auth()->user()->id === $developerProfile->user_id || (auth()->user()->companyProfile && auth()->user()->companyProfile->paid_subscription)))
+                            <p class="text-xl">{{ $developerProfile->user->name  }}</p>
+                        @endif
+                    </div>
+
+                    <div class="mb-4 rounded" id="bio-container"></div>
+
+                    <h3 class="text-lg mb-1">Social Links</h3>
+
+                    @if (auth()->check() && ($developerProfile->user_id === auth()->user()->id || (auth()->user()->companyProfile && auth()->user()->companyProfile->paid_subscription)))
+                        <div class="grid gap-4">
+                            <div>
+                                @if($developerProfile->website)
+                                    <p class="text-gray-800 mb-2"><a href="{{ $developerProfile->website }}" class="text-gray-500 inline-flex items-center"><i class="fa-solid fa-globe mr-2"></i> {{ $developerProfile->website }}</a></p>
+                                @endif
+
+                                @if($developerProfile->github)
+                                    <p class="text-gray-800 mb-2"><a href="https://github.com/{{ $developerProfile->github }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-github mr-2"></i>  {{ $developerProfile->github }}</a></p>
+                                @endif
+
+                                @if($developerProfile->stack_overflow)
+                                    <p class="text-gray-800 mb-2"><a href="https://stackoverflow.com/users/{{ $developerProfile->stack_overflow }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-stack-overflow mr-2"></i> {{ $developerProfile->stack_overflow }}</a></p>
+                                @endif
+
+                                @if($developerProfile->linkedin)
+                                    <p class="text-gray-800 mb-2"><a href="https://www.linkedin.com/in/{{ $developerProfile->linkedin }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-linkedin mr-2"></i> {{ $developerProfile->linkedin }}</a></p>
+                                @endif
+
+                                @if($developerProfile->twitter)
+                                    <p class="text-gray-800 mb-2"><a href="https://twitter.com/{{ $developerProfile->twitter }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-twitter mr-2"></i> {{ $developerProfile->twitter }}</a></p>
+                                @endif
+                            </div>
+                        </div>
+                    @else
+
+                    <span class="inline-block border-dashed border-2 border-gray-400 bg-gray-200 rounded-lg px-3 py-1.5 mb-5">
+                        <span class="inline-block pr-1">
+                          <svg class="fill-current text-gray-500 w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"></path>
+                          </svg>
+                         </span>
+                        Private Info
+                     </span>
                     @endif
                 </div>
+                <div class="col-span-1 md:col-span-1 border-2 border-customBlue rounded-lg" style="height: fit-content;">
+                    <div class="flex flex-wrap flex-row items-start space-y-3 p-6">
 
-                <div class="sm:grid sm:grid-cols-2 flex flex-col">
-                    <!-- Location -->
-                    <div class="mb-4">
-                        <h3 class="text-lg mb-1">Location</h3>
-                        <p><i class="fa-solid fa-location-dot text-gray-400 mr-2"></i>{{ $developerProfile->city }}, {{ $developerProfile->state }}</p>
-                    </div>
-
-                    <!-- Search Status -->
-                    <div class="mb-4">
-                        <h3 class="text-lg">Search Status</h3>
-                        <p><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>{{ ucfirst($developerProfile->search_status) }}</p>
-                    </div>
-
-                    <!-- Preferred Positions -->
-                    <div class="mb-4">
-                        <h3 class="text-lg mb-1">Preferred Positions</h3>
-                        @if($developerProfile->part_time)
-                            <p class="text-gray-800"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>Part-time</p>
+                        @if(!auth()->user()?->companyProfile?->paid_subscription)
+                            <a href="/hire" class="transition-all group duration-200 ease-in-out bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg py-3 px-5 inline-flex items-center justify-start w-full">
+                                <div class="z-10 text-left">
+                                    <p class="mb-2 font-bold text-gray-500">Private Information</p>
+                                    <p class="mb-2 text-gray-500">This information is only visible with a business subscription.</p>
+                                    <p class="text-sm font-bold text-gray-500">Subscribe →</p>
+                                </div>
+                            </a>
                         @endif
-                        @if($developerProfile->full_time)
-                            <p class="text-gray-800"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>Full-time</p>
-                        @endif
-                        @if($developerProfile->contract)
-                            <p class="text-gray-800"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>Contract</p>
-                        @endif
-                    </div>
-
-                    <!-- Role Level -->
-                    <div class="mb-4">
-                        <h3 class="text-lg mb-1">Role Level</h3>
-                        <p class="text-gray-800 mb-2"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>{{ ucfirst($developerProfile->role_level) }}</p>
-                    </div>
-
-                </div>
-
-                <h3 class="text-lg mb-1">Bio</h3>
-                <div class="mb-4 border rounded px-4" id="bio-container"></div>
-
-                <h3 class="text-lg mb-1">Social Links</h3>
-
-                @if (auth()->check() && ($developerProfile->user_id === auth()->user()->id || (auth()->user()->companyProfile && auth()->user()->companyProfile->paid_subscription)))
-                    <div class="grid gap-4">
-                        <div>
-                            @if($developerProfile->website)
-                                <p class="text-gray-800 mb-2"><a href="{{ $developerProfile->website }}" class="text-gray-500 inline-flex items-center"><i class="fa-solid fa-globe mr-2"></i> {{ $developerProfile->website }}</a></p>
+                        <div class="w-full">
+                            <h5 class="font-medium text-black mb-1">Search status</h5>
+                            <p class="text-gray-600 text-base inline-flex items-center">
+                                <i class="fa-solid fa-circle-check text-gray-400 mr-2"></i><span>{{ ucfirst($developerProfile->search_status) }}</span>
+                            </p>
+                        </div>
+                        <div class="w-full">
+                            <h5 class="font-medium text-black mb-1">Experience level</h5>
+                            <p class="text-gray-600 text-base inline-flex items-center">
+                                <i class="fa-solid fa-circle-check text-gray-400 mr-2"></i><span>{{ ucfirst($developerProfile->role_level) }}</span>
+                            </p>
+                        </div>
+                        <div class="w-full">
+                            <h5 class="font-medium text-black mb-1">Location</h5>
+                            <p class="text-gray-600 text-base inline-flex items-center">
+                                <i class="fa-solid fa-location-dot text-gray-400 mr-2"></i>
+                                <span>{{ $developerProfile->city }}, {{ $developerProfile->state }}</span>
+                            </p>
+                        </div>
+                        <div class="w-full">
+                            <h5 class="font-medium text-black mb-1">Preferred Positions</h5>
+                            @if($developerProfile->part_time)
+                                <p class="text-gray-800"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>Part-time</p>
                             @endif
-
-                            @if($developerProfile->github)
-                                <p class="text-gray-800 mb-2"><a href="https://github.com/{{ $developerProfile->github }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-github mr-2"></i>  {{ $developerProfile->github }}</a></p>
+                            @if($developerProfile->full_time)
+                                <p class="text-gray-800"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>Full-time</p>
                             @endif
-
-                            @if($developerProfile->stack_overflow)
-                                <p class="text-gray-800 mb-2"><a href="https://stackoverflow.com/users/{{ $developerProfile->stack_overflow }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-stack-overflow mr-2"></i> {{ $developerProfile->stack_overflow }}</a></p>
-                            @endif
-
-                            @if($developerProfile->linkedin)
-                                <p class="text-gray-800 mb-2"><a href="https://www.linkedin.com/in/{{ $developerProfile->linkedin }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-linkedin mr-2"></i> {{ $developerProfile->linkedin }}</a></p>
-                            @endif
-
-                            @if($developerProfile->twitter)
-                                <p class="text-gray-800 mb-2"><a href="https://twitter.com/{{ $developerProfile->twitter }}" class="text-gray-500 inline-flex items-center"><i class="fa-brands fa-twitter mr-2"></i> {{ $developerProfile->twitter }}</a></p>
+                            @if($developerProfile->contract)
+                                <p class="text-gray-800"><i class="fa-solid fa-circle-check text-gray-400 mr-2"></i>Contract</p>
                             @endif
                         </div>
                     </div>
-                @else
-                 <span class=" inline-block border-dashed border-2 border-gray-400 bg-gray-200 rounded-lg px-3 py-1.5">
-                    <span class="inline-block pr-1">
-                      <svg class="fill-current text-gray-500 w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"></path>
-                      </svg>
-                    </span>
-                    Private Info
-                 </span>
-                @endif
+                    <div class="flex items-center">
+                        @if(auth()->user()?->companyProfile?->paid_subscription && auth()->user()->id !== $developerProfile->user_id)
+                            <a href="{{ url('/messages/' . $developerProfile->user_id) }}" class="text-base font-semibold text-white bg-customBlue leading-loose relative flex items-center justify-center py-1 px-4 mx-auto w-full">
+                                <i class="fas fa-comment-dots mr-1"></i> Message
+                            </a>
+                        @endif
+
+                        @if (!auth()->user()?->companyProfile?->paid_subscription)
+                            <a href="/hire" class="text-base font-semibold text-white bg-customBlue leading-loose relative flex items-center justify-center py-1 px-4 mx-auto w-full">
+                                Subscribe to hire →
+                            </a>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>
