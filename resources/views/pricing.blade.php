c528adedb7a0698fcc67a5bb4c660503832642df
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-08-11 19:31:16 +1000

    Update text

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -4,13 +4,12 @@
     <div>
         <div class="mx-auto max-w-7xl px-2 lg:px-8">
             <div class="mx-auto max-w-2xl sm:text-center">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple no-tricks pricing</h2>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Find talented developers in Australia</h2>
                 <p class="mt-6 text-lg leading-8 text-gray-600">Directly connect with hundreds of developers in Australia looking for their next job.</p>
             </div>
             <div class="bg-white mx-auto mt-8 md:mt-14 max-w-2xl rounded-lg ring-1 ring-gray-200 lg:mx-0 lg:flex lg:max-w-none">
                 <div class="p-8 sm:p-10 lg:flex-auto">
-                    <h3 class="text-2xl font-bold tracking-tight text-gray-900">Monthly Subscription</h3>
-                    <p class="mt-6 text-base leading-7 text-gray-600">Skip the formal job postings and middlemen – hire directly from a candidate pool of developers in Australia, from juniors to seniors.</p>
+                    <h3 class="text-2xl font-bold tracking-tight text-gray-900">Save time & money</h3>
                     <div class="mt-10 flex items-center gap-x-4">
                         <h4 class="flex-none font-semibold leading-6">What’s included</h4>
                         <div class="h-px flex-auto bg-gray-100"></div>
