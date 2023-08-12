e7df99ead1624933d55ee856d7020e9c2cbffb6a
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-08-12 12:44:41 +1000

    test

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            purge: [
+                './resources/**/*.{js,ts,jsx,tsx,php,blade.php}', // Include JavaScript, TypeScript, PHP, and Blade files within the resources directory
+            ],
             refresh: true
         }),
     ],
