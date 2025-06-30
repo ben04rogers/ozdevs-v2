1098b45ab43c4d34bd4d117e128dbe75b9daacf2
Ben Rogers <benrogers1299@outlook.com>
2025-07-01 09:13:23 +1000

    Add phpdoc block to ChFavorite model

--- app/Models/ChFavorite.php
+++ app/Models/ChFavorite.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Chatify\Traits\UUID;
 
+/**
+ * @property-read \App\Models\User|null $user
+ */
 class ChFavorite extends Model
 {
     use UUID;
