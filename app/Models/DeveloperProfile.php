2a1dad2656e738cbb7b102fc7b5a11d8e12eb925
ben04rogers <benrogers1299@outlook.com>
2025-07-15 22:15:57 +1000

    Fix phpstan error

--- app/Models/DeveloperProfile.php
+++ app/Models/DeveloperProfile.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
  * @property int $user_id
  * @property string|null $name
  * @property string|null $hero
